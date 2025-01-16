283f5e3053124e40f77e8c9bacf82db73292b3f2
David G <48044383+gradav@users.noreply.github.com>
2025-01-16 17:13:00 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,58 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-provider "aws" {
-  region = var.region
+provider "google" {
+  project = "your-project-id"
+  region  = "us-central1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "google_compute_instance" "sftp_server" {
+  name         = "sftp-server"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  network_interface {
+    network       = "default"
+    access_config {
+      // Allocates an external IP
+    }
   }
 
-  owners = ["099720109477"] # Canonical
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update
+    apt-get install -y openssh-server
+    mkdir -p /var/sftp/uploads
+    groupadd sftpusers
+    useradd -m -d /home/sftpuser -s /usr/sbin/nologin -G sftpusers sftpuser
+    echo 'sftpuser:password' | chpasswd
+    chown root:root /var/sftp
+    chmod 755 /var/sftp
+    chown sftpuser:sftpusers /var/sftp/uploads
+    echo 'Match Group sftpusers' >> /etc/ssh/sshd_config
+    echo '    ChrootDirectory /var/sftp' >> /etc/ssh/sshd_config
+    echo '    ForceCommand internal-sftp' >> /etc/ssh/sshd_config
+    echo '    AllowTcpForwarding no' >> /etc/ssh/sshd_config
+    echo '    X11Forwarding no' >> /etc/ssh/sshd_config
+    systemctl restart sshd
+  EOT
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+resource "google_compute_firewall" "allow_sftp" {
+  name    = "allow-sftp"
+  network = "default"
 
-  tags = {
-    Name = var.instance_name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
   }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "sftp_static_ip" {
+  name = "sftp-static-ip"
 }
